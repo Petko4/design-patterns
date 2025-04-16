33de1219ed60fca0d096f612fc53dc2f7854d0d2
Petko4 <petr.vaclavik4@gmail.com>
2025-04-16 07:32:52 +0200

    SimpleFactoryMethod

---
+++ SimpleFactoryMethod/APerson.cs
@@ -0,0 +1,84 @@
+abstract public class APerson
+{
+    private static int index = 0;
+
+    public static APerson GetPerson()
+    {
+        return (index++ % 3) switch
+        {
+            0 => new LazyPerson(),
+            1 => new BriskPerson(),
+            2 => new HardWorkingPerson(),
+            _ => throw new Exception(),
+        };
+    }
+
+    abstract public void WakeUp();
+    abstract public void Work();
+    abstract public void FreeTime();
+    abstract public void Sleep();
+
+    private class LazyPerson : APerson
+    {
+        public override void WakeUp()
+        {
+            Console.WriteLine("Slowly wake up.");
+        }
+        public override void Work()
+        {
+            Console.WriteLine("Lazy work.");
+        }
+        public override void FreeTime()
+        {
+            Console.WriteLine("Going to sleep.");
+        }
+
+        public override void Sleep()
+        {
+            Console.WriteLine("Still sleeping");
+        }
+    }
+
+    private class BriskPerson : APerson
+    {
+        public override void WakeUp()
+        {
+            Console.WriteLine("Fast wake up.");
+        }
+        public override void Work()
+        {
+            Console.WriteLine("Brisk work.");
+        }
+        public override void FreeTime()
+        {
+            Console.WriteLine("Active rest.");
+        }
+        public override void Sleep()
+        {
+            Console.WriteLine("Pass out and sleep.");
+        }
+    }
+
+    private class HardWorkingPerson : APerson
+    {
+        public override void WakeUp()
+        {
+            Console.WriteLine("Waking up early.");
+        }
+        public override void Work()
+        {
+            Console.WriteLine("Hard working.");
+        }
+        public override void FreeTime()
+        {
+            Console.WriteLine("Still working.");
+        }
+        public override void Sleep()
+        {
+            Console.WriteLine("Falling asleep at work.");
+        }
+    }
+
+
+
+}
---
+++ SimpleFactoryMethod/Program.cs
@@ -0,0 +1,15 @@
+﻿static void test()
+{
+    for (int i = 1; i <= 3; i++)
+    {
+        APerson p = APerson.GetPerson();
+        Console.WriteLine("\n=== New person: " + p.GetType().Name);
+        p.WakeUp();
+        p.Work();
+        p.FreeTime();
+        p.Sleep();
+    }
+}
+
+
+test();
